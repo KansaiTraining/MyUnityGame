a1417525f5d560e2a5d766ed032142b653e9547f
Cristian Fuentes <adapt.robot.lab@gmail.com>
2017-11-27 17:54:42 +0900

    initial local commit

---
+++ Assets/Scripts/Auxiliar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auxiliar{
+
+	public int GivemeSomething()
+	{
+
+		return 10;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+	private Auxiliar aux;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+		aux = new Auxiliar ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //before performing any physics calculation
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		speed = aux.GivemeSomething ();
+		rb.AddForce(movement*speed);
+    }
+}
